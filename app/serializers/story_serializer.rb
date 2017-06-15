eb65567ebfa28796395b02296867e082f971d36d
Nilay <nilayanand@gmail.com>
2017-06-15 14:06:41 +0530

    Refactor story db structure

--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -1,3 +1,37 @@
 class StorySerializer < ActiveModel::Serializer
-	attributes :id, :user_id, :caption, :likes_count, :face1_thumb, :face2_thumb, :face3_thumb, :face4_thumb, :story_face1, :story_face2, :story_face3, :story_face4, :active
+	attributes :id, :user_id, :caption, :likes_count, :comments_count, :media
+
+	def media
+		[
+			{
+				media_url: object.face1_media.url, 
+				thumb_url: object.face1_media.content_type == 'video/mp4' ? object.face1_video_thumb.url(:thumb) : object.face1_media.url(:thumb)  , 
+				media_type:object.face1_media.content_type
+			},
+
+			{
+				media_url: object.face2_media.url, 
+				thumb_url: object.face2_media.content_type == 'video/mp4' ? object.face2_video_thumb.url(:thumb) : object.face2_media.url(:thumb)  , 
+				media_type:object.face2_media.content_type
+			},
+
+			{
+				media_url: object.face3_media.url, 
+				thumb_url: object.face3_media.content_type == 'video/mp4' ? object.face3_video_thumb.url(:thumb) : object.face3_media.url(:thumb)  , 
+				media_type:object.face3_media.content_type
+			},
+
+			{
+				media_url: object.face4_media.url, 
+				thumb_url: object.face4_media.content_type == 'video/mp4' ? object.face4_video_thumb.url(:thumb) : object.face4_media.url(:thumb)  , 
+				media_type:object.face4_media.content_type
+			},
+
+
+
+		]
+
+	end		
+
+
 end
