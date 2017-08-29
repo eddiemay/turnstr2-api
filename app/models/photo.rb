8c4f8542c949aee195a63bf8d3de68f5fcc23fd2
Nilay <nilayanand@gmail.com>
2017-08-30 00:53:07 +0530

    Add cover image when photo is uploaded

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -14,12 +14,17 @@ class Photo < ApplicationRecord
 	validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
 
 	before_create :set_captured_date
+	after_create :set_cover_image
 
 	scope :grouped, -> {select("concat(year(captured_date), month(captured_date)) as month_group").group("concat(year(captured_date), month(captured_date))")}
 
 	def set_captured_date
 		self.captured_date = Date.today unless self.captured_date
 	end
+
+	def set_cover_image
+		album.update_attribute(:cover_image_url, image.url(:thumb))
+  end		
 
 		
 end
