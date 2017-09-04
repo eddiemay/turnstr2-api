f756087ad31ac081d8f57f26e6e6269397ea6dba
Nilay <nilayanand@gmail.com>
2017-09-04 21:32:04 +0530

    Remove default for missing image from story

--- app/models/story.rb
+++ app/models/story.rb
@@ -9,19 +9,19 @@ class Story < ApplicationRecord
   has_many :likes, as: :likable
   
  # Paperclip configuration for adding image to User
-  has_attached_file :face1_video_thumb, styles: { thumb: "300x300>"}, default_url: "http://#{Rails.application.secrets.host}/images/missing.png"
-  has_attached_file :face2_video_thumb, styles: { thumb: "300x300>"}, default_url: "http://#{Rails.application.secrets.host}/images/missing.png"
-  has_attached_file :face3_video_thumb, styles: { thumb: "300x300>"}, default_url: "http://#{Rails.application.secrets.host}/images/missing.png"
-  has_attached_file :face4_video_thumb, styles: { thumb: "300x300>"}, default_url: "http://#{Rails.application.secrets.host}/images/missing.png"
-  has_attached_file :face5_video_thumb, styles: { thumb: "300x300>"}, default_url: "http://#{Rails.application.secrets.host}/images/missing.png"
-  has_attached_file :face6_video_thumb, styles: { thumb: "300x300>"}, default_url: "http://#{Rails.application.secrets.host}/images/missing.png"
+  has_attached_file :face1_video_thumb, styles: { thumb: "300x300>"}
+  has_attached_file :face2_video_thumb, styles: { thumb: "300x300>"}
+  has_attached_file :face3_video_thumb, styles: { thumb: "300x300>"}
+  has_attached_file :face4_video_thumb, styles: { thumb: "300x300>"}
+  has_attached_file :face5_video_thumb, styles: { thumb: "300x300>"}
+  has_attached_file :face6_video_thumb, styles: { thumb: "300x300>"}
 
-  has_attached_file :face1_media, styles: { thumb: "300x300>"}, default_url: "http://#{Rails.application.secrets.host}/images/missing.png"
-  has_attached_file :face2_media, styles: { thumb: "300x300>"}, default_url: "http://#{Rails.application.secrets.host}/images/missing.png"
-  has_attached_file :face3_media, styles: { thumb: "300x300>"}, default_url: "http://#{Rails.application.secrets.host}/images/missing.png"
-  has_attached_file :face4_media, styles: { thumb: "300x300>"}, default_url: "http://#{Rails.application.secrets.host}/images/missing.png"
-  has_attached_file :face5_media, styles: { thumb: "300x300>"}, default_url: "http://#{Rails.application.secrets.host}/images/missing.png"
-  has_attached_file :face6_media, styles: { thumb: "300x300>"}, default_url: "http://#{Rails.application.secrets.host}/images/missing.png"
+  has_attached_file :face1_media, styles: { thumb: "300x300>"}
+  has_attached_file :face2_media, styles: { thumb: "300x300>"}
+  has_attached_file :face3_media, styles: { thumb: "300x300>"}
+  has_attached_file :face4_media, styles: { thumb: "300x300>"}
+  has_attached_file :face5_media, styles: { thumb: "300x300>"}
+  has_attached_file :face6_media, styles: { thumb: "300x300>"}
 
   validates :face1_media, :face2_media, :face3_media, :face4_media, attachment_presence: true
   validates_attachment_content_type :face1_media, :face2_media, :face3_media, :face4_media, content_type: ['image/jpeg', 'image/png', 'video/mp4']
