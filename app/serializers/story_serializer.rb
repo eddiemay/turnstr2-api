ab637fff512673c9ea33dbbea7983ba6e9659948
Nilay <nilayanand@gmail.com>
2017-09-04 22:18:52 +0530

    Remove default imag from turn story

--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -6,38 +6,50 @@ class StorySerializer < ActiveModel::Serializer
 	def media
 		[
 			{
-				media_url: object.face1_media.url, 
-				thumb_url: object.face1_media.content_type == 'video/mp4' ? object.face1_video_thumb.url(:thumb) : object.face1_media.url(:thumb)  , 
+				media_url: object.face1_media.file? ? object.face1_media.url : nil, 
+				thumb_url: object.face1_media.content_type == 'video/mp4' ? 
+					object.face1_video_thumb.file? ? object.face1_video_thumb.url(:thumb) : nil 
+					: object.face1_media.file? ? object.face1_media.url(:thumb) : nil , 
 				media_type:object.face1_media.content_type
 			},
 
 			{
-				media_url: object.face2_media.url, 
-				thumb_url: object.face2_media.content_type == 'video/mp4' ? object.face2_video_thumb.url(:thumb) : object.face2_media.url(:thumb)  , 
+				media_url: object.face2_media.file? ? object.face2_media.url : nil,  
+				thumb_url: object.face2_media.content_type == 'video/mp4' ? 
+					object.face2_video_thumb.file? ? object.face2_video_thumb.url(:thumb) : nil 
+					: object.face2_media.file? ? object.face2_media.url(:thumb) : nil , 
 				media_type:object.face2_media.content_type
 			},
 
 			{
-				media_url: object.face3_media.url, 
-				thumb_url: object.face3_media.content_type == 'video/mp4' ? object.face3_video_thumb.url(:thumb) : object.face3_media.url(:thumb)  , 
+				media_url: object.face3_media.file? ? object.face3_media.url : nil, 
+				thumb_url: object.face3_media.content_type == 'video/mp4' ? 
+					object.face3_video_thumb.file? ? object.face3_video_thumb.url(:thumb) : nil 
+					: object.face3_media.file? ? object.face3_media.url(:thumb) : nil , 
 				media_type:object.face3_media.content_type
 			},
 
 			{
-				media_url: object.face4_media.url, 
-				thumb_url: object.face4_media.content_type == 'video/mp4' ? object.face4_video_thumb.url(:thumb) : object.face4_media.url(:thumb)  , 
-				media_type:object.face4_media.content_type
+				media_url: object.face4_media.file? ? object.face4_media.url : nil, 
+					thumb_url: object.face4_media.content_type == 'video/mp4' ? 
+					object.face4_video_thumb.file? ? object.face4_video_thumb.url(:thumb) : nil 
+					: object.face4_media.file? ? object.face4_media.url(:thumb) : nil , 
+			media_type:object.face4_media.content_type
 			},
 
 			{
-				media_url: object.face5_media.url, 
-				thumb_url: object.face5_media.content_type == 'video/mp4' ? object.face5_video_thumb.url(:thumb) : object.face5_media.url(:thumb)  , 
+				media_url: object.face5_media.file? ? object.face5_media.url : nil, 
+					thumb_url: object.face5_media.content_type == 'video/mp4' ? 
+					object.face5_video_thumb.file? ? object.face5_video_thumb.url(:thumb) : nil 
+					: object.face5_media.file? ? object.face5_media.url(:thumb) : nil , 
 				media_type:object.face5_media.content_type
 			},
 
 			{
-				media_url: object.face6_media.url, 
-				thumb_url: object.face6_media.content_type == 'video/mp4' ? object.face6_video_thumb.url(:thumb) : object.face6_media.url(:thumb)  , 
+				media_url: object.face6_media.file? ? object.face6_media.url : nil, 
+				thumb_url: object.face6_media.content_type == 'video/mp4' ? 
+					object.face6_video_thumb.file? ? object.face6_video_thumb.url(:thumb) : nil 
+					: object.face6_media.file? ? object.face6_media.url(:thumb) : nil , 
 				media_type:object.face6_media.content_type
 			}
 
