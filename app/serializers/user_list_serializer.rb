23c515c638907bc767b9dd5ef27077f61be41402
Nilay <nilayanand@gmail.com>
2017-12-06 18:26:48 +0530

    Add is_verified column in response

--- app/serializers/user_list_serializer.rb
+++ app/serializers/user_list_serializer.rb
@@ -1,5 +1,6 @@
 class UserListSerializer < ActiveModel::Serializer
-    attributes :id, :username, :first_name, :last_name, :avatar_face1, :avatar_face2, :avatar_face3, :avatar_face4, :avatar_face5, :avatar_face6, :online
+    attributes :id, :username, :first_name, :last_name, :avatar_face1, :avatar_face2, :avatar_face3,
+               :avatar_face4, :avatar_face5, :avatar_face6, :online, :is_verified
 
 
   %w(avatar_face1 avatar_face2 avatar_face3 avatar_face4 avatar_face5 avatar_face6).each do |attr|
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :username, :first_name, :last_name, :phone, :avatar_face1, :avatar_face2, :avatar_face3, :avatar_face4, :avatar_face5, :avatar_face6, :website, :bio, :gender, :address, :city, :state, :post_count, :following_count, :follower_count, :family_count, :info, :contact_me, :online, :is_verified
+  attributes :id, :email, :username, :first_name, :last_name, :phone, :avatar_face1, :avatar_face2, :avatar_face3, :avatar_face4, :avatar_face5,
+             :avatar_face6, :website, :bio, :gender, :address, :city, :state, :post_count, :following_count, :follower_count, :family_count,
+             :info, :contact_me, :online, :is_verified
 
   has_one :live_session
 
