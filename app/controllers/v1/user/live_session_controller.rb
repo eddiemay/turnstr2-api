b6a4cc84db95c3b81638df323ed8d9ee59996248
Nilay <nilayanand@gmail.com>
2017-12-19 16:24:39 +0530

    fix live session

--- app/controllers/v1/user/live_session_controller.rb
+++ app/controllers/v1/user/live_session_controller.rb
@@ -11,11 +11,12 @@ class V1::User::LiveSessionController < V1::User::BaseController
   # POST /user/session
   def create
 
-    if current_user.create_live_session
+    if live_session = current_user.create_live_session
+      current_user.reload
       # invite (send push notification) to invitee users so they can join the live session
       current_user.invite_users_to_my_live_session(params[:invitees], params[:call_type])
 
-      render_success data: {session: current_user.live_session}, message: I18n.t('resource.crated', resource: LiveSession.model_name.human)
+      render_success data: {session: live_session}, message: I18n.t('resource.crated', resource: LiveSession.model_name.human)
     else
       render_unprocessable_entity message: current_user.errors.full_messages.join(', ')
     end
