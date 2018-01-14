0a265bdc68ea564e60e37336f6ac16acc189f971
Nilay <nilayanand@gmail.com>
2018-01-14 22:24:07 +0530

    Record live video

--- app/controllers/v1/opentok_callback_controller.rb
+++ app/controllers/v1/opentok_callback_controller.rb
@@ -9,18 +9,19 @@ class V1::OpentokCallbackController < V1::BaseController
 
   # POST /v1/sessions
   def create
-    if go_live_video = GoLiveVideo.create({
-                                              opentok_id: params[:id],
-                                              event: params[:event],
-                                              opentok_created_at: params[:createdAt],
-                                              duration: params[:duration],
-                                              partner_id: params[:partnerId],
-                                              reason: params[:reason],
-                                              opentok_session_id: params[:sessionId],
-                                              size: params[:size],
-                                              status: params[:status],
-                                              url: params[:url]
-                                          })
+    go_live_video = GoLiveVideo.new({
+                                           opentok_id: params[:id],
+                                           event: params[:event],
+                                           opentok_created_at: params[:createdAt],
+                                           duration: params[:duration],
+                                           partner_id: params[:partnerId],
+                                           reason: params[:reason],
+                                           opentok_session_id: params[:sessionId],
+                                           size: params[:size],
+                                           status: params[:status],
+                                           url: params[:url]
+                                       })
+    if go_live_video.save
       render_success message: "Saved", data: { go_live_video: go_live_video }
     else
       render_unprocessable_entity message: go_live_video.errors.full_messages.join(', ')
--- app/models/go_live_video.rb
+++ app/models/go_live_video.rb
@@ -4,7 +4,7 @@ class GoLiveVideo < ApplicationRecord
   include CanBeCommented
   default_scope { order(created_at: :desc) }
 
-  belongs_to :live_session
+  # belongs_to :live_session
 
   has_many :comments, as: :commentable
   has_many :likes, as: :likable
