7dcb7e6a59e51f7df895c7cefc764abec2766f69
Nilay <nilayanand@gmail.com>
2018-01-25 00:58:12 +0530

    cloud url for recorded videos

--- app/models/go_live_video.rb
+++ app/models/go_live_video.rb
@@ -1,5 +1,6 @@
 class GoLiveVideo < ApplicationRecord
 
+  # https://s3-us-west-2.amazonaws.com/turnstr2-production/45910392/012b1572-95cf-42d0-9802-e6dae5d06aeb/archive.mp4
   include CanBeLiked
   include CanBeCommented
   default_scope { order(created_at: :desc) }
@@ -8,5 +9,12 @@ class GoLiveVideo < ApplicationRecord
 
   has_many :comments, as: :commentable
   has_many :likes, as: :likable
+
+  scope :uploaded, -> {where(status: 'uploaded').order("created_at desc")}
+
+
+  def url
+    'https://' + Rails.application.secrets.s3_host_name + '/' + Rails.application.secrets.s3_bucket  + '/' + Rails.application.config.open_tok_api_key + '/' + (opentok_session_id || '') +  '/archive.mp4'
+  end
 
 end
